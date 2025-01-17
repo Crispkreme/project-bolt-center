4161af3767a07f40595bdddd220f67a6f3a5463f
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2025-01-17 16:20:47 +0800

    updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="account-content">
+        <div class="login-wrapper bg-img">
+            <div class="login-content">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="login-userset">
+                        {{-- <div class="login-logo logo-normal">
+                                <img src="https://dreamspos.dreamstechnologies.com/laravel/template/public/build/img/logo.png" alt="img">
+                            </div>
+                            <a href="https://dreamspos.dreamstechnologies.com/laravel/template/public/index" class="login-logo logo-white">
+                                <img src="https://dreamspos.dreamstechnologies.com/laravel/template/public/build/img/logo-white.png" alt="">
+                            </a> --}}
+                        <div class="login-userheading">
+                            <h3>Sign In</h3>
+                            <h4>Access the Dreamspos panel using your email and passcode.</h4>
+                        </div>
+                        <div class="form-login mb-3">
+                            <label class="form-label">Email Address</label>
+                            <div class="form-addons">
+                                <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="youremail@email.com">
+                                <img src="{{ asset('images/svg/mail.svg') }}" alt="img">
+                            </div>
+                            <div class="text-danger pt-2"></div>
+                        </div>
+                        <div class="form-login mb-3">
+                            <label class="form-label">Password</label>
+                            <div class="pass-group">
+                                <input type="password" class="pass-input form-control" id="password" name="password"
+                                    value="123456">
+                                <span class="fas toggle-password fa-eye-slash"></span>
+                            </div>
+                            <div class="text-danger pt-2"></div>
+                        </div>
+                        <div class="form-login authentication-check">
+                            <div class="row">
+                                <div class="col-12 d-flex align-items-center justify-content-between">
+                                    <div class="custom-control custom-checkbox">
+                                        <label class="checkboxs ps-4 mb-0 pb-0 line-height-1">
+                                            <input type="checkbox" class="form-control">
+                                            <span class="checkmarks"></span>
+                                            Remember me
+                                        </label>
+                                    </div>
+                                    <div class="text-end">
+                                        <a class="forgot-link" href="{{ route('password.request') }}">Forgot
+                                            Password?</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-login">
+                            <button type="submit" class="btn btn-login">Sign In</button>
+                        </div>
+                        <div class="signinform">
+                            <h4>New on our platform?<a
+                                    href="https://dreamspos.dreamstechnologies.com/laravel/template/public/register"
+                                    class="hover-a"> Create an account</a>
+                            </h4>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
